0fc7530c5bbc4af7206a659ab2aa5ff45be03b10
지찬우 <jcw001031@gmail.com>
2023-02-20 21:51:11 +0900

    initial commit

---
+++ src/main/java/com/woopaca/newcamo/NewCamoApplication.java
@@ -0,0 +1,13 @@
+package com.woopaca.newcamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCamoApplication.class, args);
+	}
+
+}
