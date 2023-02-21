d66ba5dbcdef01010d2d72bc20ea03c8ff1ed0ed
지찬우 <jcw001031@gmail.com>
2023-02-21 17:36:36 +0900

    JWT 발급 및 Utils 클래스 개발

--- src/main/java/com/woopaca/newcamo/config/jwt/JwtUtils.java
+++ src/main/java/com/woopaca/newcamo/config/jwt/JwtUtils.java
@@ -1,18 +1,66 @@
 package com.woopaca.newcamo.config.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
 public class JwtUtils {
 
-    public String resolveToken(String header) {
-        return null;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String resolveToken(String authorization) {
+        if (authorization == null) {
+            return null;
+        }
+        if (!authorization.startsWith("Bearer ")) {
+            throw new IllegalArgumentException();
+        }
+
+        return authorization.split(" ")[1];
     }
 
     public boolean isNotExpired(String token) {
-        return false;
+        Jws<Claims> claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token);
+
+        return !claims.getBody()
+                .getExpiration()
+                .before(new Date());
     }
 
     public Authentication getAuthentication(String token) {
-        return null;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUsernameInToken(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "",
+                userDetails.getAuthorities());
+    }
+
+    public String getUsernameInToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody().getSubject();
     }
 }
