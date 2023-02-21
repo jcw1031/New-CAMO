b7112a6e45f48b978860b1804ceb242abae02d15
지찬우 <jcw001031@gmail.com>
2023-02-21 17:40:39 +0900

    UserDetailsService 구현

---
+++ src/main/java/com/woopaca/newcamo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.woopaca.newcamo.service;
+
+import com.woopaca.newcamo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String studentId) throws UsernameNotFoundException {
+        return userRepository.findByEmail(studentId).orElse(null);
+    }
+}
