6d94e7325d94ff6b586472a54ee1f7f25b3dcced
지찬우 <jcw001031@gmail.com>
2023-02-21 16:25:41 +0900

    Spring Security 커스텀 기본 설정

---
+++ src/main/java/com/woopaca/newcamo/config/secure/AuthenticationConfig.java
@@ -0,0 +1,40 @@
+package com.woopaca.newcamo.config.secure;
+
+import com.woopaca.newcamo.config.jwt.JwtAuthenticationFilter;
+import com.woopaca.newcamo.exception.handler.auth.CustomAccessDeniedHandler;
+import com.woopaca.newcamo.exception.handler.auth.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/users/sign-up", "/api/v1/users/sign-in").permitAll()
+                .antMatchers("/api/v1/**").permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
