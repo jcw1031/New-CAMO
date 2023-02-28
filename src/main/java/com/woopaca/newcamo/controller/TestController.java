4bfc4fcf54b4932fdbfad8a5d69c70e898071087
지찬우 <jcw001031@gmail.com>
2023-02-28 22:01:47 +0900

    권한 설정 테스트

---
+++ src/main/java/com/woopaca/newcamo/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.woopaca.newcamo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employments")
+public class TestController {
+
+    @GetMapping("")
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok().body("GET TEST");
+    }
+}
--- src/main/java/com/woopaca/newcamo/controller/dto/cafe/CafeRegisterRequestDto.java
+++ src/main/java/com/woopaca/newcamo/controller/dto/cafe/CafeRegisterRequestDto.java
@@ -4,11 +4,15 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CafeRegisterRequestDto {
 
+    @NotBlank(message = "카페 이름은 비어있을 수 없습니다.")
     private String cafeName;
+    @NotBlank(message = "카페 주소는 비어있을 수 없습니다.")
     private String cafeAddress;
     private String cafePhone;
     private String cafeIntroduction;
